51752809e283f455614352c8c44afde548be4abf
Christopher Hanke <christopherhanke@googlemail.com>
2020-11-22 10:06:25 +0100

    starting with Vampire class

---
+++ vampire.py
@@ -0,0 +1,62 @@
+class Vampire:
+    clans = ["Brujah", "Gangrel", "Malkavian", "Nosferatu", "Toreador", "Tremere", "Ventrue"]
+    
+    attributes = {
+        "Strength": None,
+        "Dexterity": None,
+        "Stamina": None,
+        "Charisma": None,
+        "Manipulation": None,
+        "Composure": None,
+        "Intelligence": None,
+        "Wits": None,
+        "Resolve": None
+    }
+    
+    skills = {
+        "Athletics": None,
+        "Brawl": None,
+        "Craft": None,
+        "Drive": None,
+        "Firearms": None,
+        "Larceny": None,
+        "Melee": None,
+        "Stealth": None,
+        "Survival": None,
+
+        "Animal Ken": None,
+        "Etiquette": None,
+        "Insight": None,
+        "Intimidation": None,
+        "Leadership": None,
+        "Performance": None,
+        "Persuasion": None,
+        "Streetwise": None,
+        "Subterfuge": None,
+
+        "Academics": None,
+        "Awareness": None,
+        "Finance": None,
+        "Investigation": None,
+        "Medicine": None,
+        "Occult": None,
+        "Politics": None,
+        "Science": None,
+        "Technology": None
+    }
+
+    disciplines = {
+        "Animalism": None,
+        "Auspex": None,
+        "Blood Sorcery": None,
+        "Celerity": None,
+        "Dominate": None,
+        "Fortitude": None,
+        "Obfuscate": None,
+        "Potence": None,
+        "Presence": None
+    }
+
+
+
+alex = Vampire()
