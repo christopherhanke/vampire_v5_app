7801ef002acab6f287c0223c654ef624207510d9
christopherhanke <christopherhanke@googlemail.com>
2020-11-29 19:55:10 +0100

    altering tests for dice because of changes in vampire.py

--- tests/test_vampire.py
+++ tests/test_vampire.py
@@ -1,5 +1,5 @@
 import pytest
-from vampire import Vampire, Vampire_Dices
+from vampire import Vampire
 
 @pytest.fixture
 def vamp():
@@ -13,20 +13,14 @@ def vamp_file():
     vamp_file = Vampire.new_Vampire_from_file("file_name_here")
     yield vamp_file
 
-@pytest.fixture
-def dice():
-    dice = Vampire_Dices()
-    yield dice
 
-
-def test_dice_count(dice):
-    test = dice.d10(10)
+def test_dice_count(vamp):
+    test = vamp.d10(10)
     assert len(test) == 10
 
-
-def test_dice_num():
+def test_dice_num(vamp):
     for x in range(100):
-        my_dice = Vampire_Dices().d10()
+        my_dice = vamp.d10()
         if (my_dice[0] < 0) or (my_dice[0] > 10):
             assert False
     assert True
@@ -39,11 +33,9 @@ def test_vampire_skill_default():
     else:
         assert False
 
-
 def test_vampire(vamp):
     attr = vamp.get_attribute(vamp.keys_attributes[1])
     assert attr == 2
-
 
 def test_vampire_file(vamp_file, capsys):
     out, err = capsys.readouterr()
